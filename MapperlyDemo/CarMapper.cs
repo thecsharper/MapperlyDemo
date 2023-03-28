22107e92f7694fd9f75038e2db36d1beeb5d87db
thecsharper <si@thecsharper.com>
2023-03-28 22:11:57 +0100

    Add project files.

---
+++ MapperlyDemo/CarMapper.cs
@@ -0,0 +1,12 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace MapperlyDemo
+{
+    // Enums of source and target have different numeric values -> use ByName strategy to map them
+    [Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName)]
+    public partial class CarMapper
+    {
+        [MapProperty(nameof(Car.Manufacturer), nameof(CarDto.Producer))] // Map property with a different name in the target type
+        public partial CarDto MapCarToDto(Car car);
+    }
+}
---
+++ MapperlyDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using MapperlyDemo;
+using FluentAssertions;
+
+var mapper = new CarMapper();
+
+var car = new Car { 
+                    Name = "Test Car", 
+                    Color = CarColor.Blue, 
+                    NumberOfSeats = 10, 
+                    Manufacturer = new Manufacturer(1, "Test Manufacturer")
+                  };
+
+var dto = mapper.MapCarToDto(car);
+
+dto.NumberOfSeats.Should().Be(10);
+
+Console.WriteLine(dto.Name);
+Console.WriteLine(dto.Color);
+Console.WriteLine(dto.NumberOfSeats);
+Console.WriteLine($"{dto.Producer?.Name} {dto.Producer?.Id}");
+Console.WriteLine(dto.Tires?.FirstOrDefault()?.Description);
